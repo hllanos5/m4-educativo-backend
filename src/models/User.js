536241f55d728d3eb51144dd74fc9fda8c95de7c
Hans Llanos <hans.llanos@gmail.com>
2024-09-19 14:42:51 -0500

    Se agrega conexion a base de datos y modelo user

---
+++ src/models/User.js
@@ -0,0 +1,46 @@
+import { Schema, model, Types } from 'mongoose'
+import Course from './Course.js'
+
+const userSchema = new Schema({
+
+    role: {
+        type: String,
+        required: true
+        },
+    name: {
+        type: String,
+        required: true
+    },
+    mName: {
+        type: String,
+        required: false
+    },
+    lName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    
+    courses: [
+        {
+        type: Types.ObjectId,
+        required: true,
+        ref: Course
+        }
+    ]
+})
+
+const User = model('User', userSchema)
+
+export default User
