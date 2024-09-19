9953a88af83970389f1a49f73f782a1ae8ad27c3
Hans Llanos <hans.llanos@gmail.com>
2024-09-19 14:45:03 -0500

    se agrega config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,8 @@
-export const config = () => {
-  return (
-    <div>config</div>
-  )
-}
+import { config } from 'dotenv'
+
+config()
+
+export const PORT = process.env.PORT || 3000
+export const DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/'
+export const SECRET_KEY = process.env.SECRET_KEY
+export const ALLOWED_ORIGINS = [undefined, 'http://localhost:5173']
