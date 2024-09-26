723929f4c68b73ea184f1b683a1d1f86c7e13468
Hans Llanos <hans.llanos@gmail.com>
2024-09-26 12:46:54 -0500

    comentario de video controller

--- src/routes/videos.routes.js
+++ src/routes/videos.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import VideoController from '../controllers/VideoController.js';
-import { handleErrors } from '../middlewares/middleware.js';
+//import VideoController from '../controllers/VideoController.js';
+//import { handleErrors } from '../middlewares/middleware.js';
 
 const router = Router();
 
-router.get('/', VideoController.getUserVideos, handleErrors);
-router.get('/:id', VideoController.getVideoById, handleErrors);
+//router.get('/', VideoController.getUserVideos, handleErrors);
+//router.get('/:id', VideoController.getVideoById, handleErrors);
 
 export default router;
